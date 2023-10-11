50e786f3d6e3d44bc4e2faf499d2b41b656b256e
zakariaesprit <zakaria.ayachi@esprit.tn>
2023-10-11 21:02:58 +0100

    Initial commit

---
+++ app/src/main/java/tn/example/gamer/MainActivity.kt
@@ -0,0 +1,37 @@
+package tn.example.gamer
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.inputmethod.InputBinding
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import org.w3c.dom.Text
+import tn.example.gamer.databinding.ActivityMainBinding
+import kotlin.math.log
+
+class MainActivity : AppCompatActivity() {
+
+private lateinit var binding: ActivityMainBinding
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var string:String
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnlogin = findViewById<Button>(R.id.button2)
+        val email = findViewById<EditText>(R.id.editTextNumber4)
+        btnlogin.setOnClickListener{
+            val alertDialogBuilder = AlertDialog.Builder(this)
+            alertDialogBuilder.setTitle("Welcome!")
+            val alertDialog = alertDialogBuilder.create()
+            alertDialog?.show()
+        }
+
+
+
+    }
+}
---
+++ app/src/main/java/tn/example/gamer/SplashActivity.kt
@@ -0,0 +1,20 @@
+package tn.example.gamer
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash)
+
+        // Start your main activity after a delay.
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000) // 2 seconds
+    }
+}
