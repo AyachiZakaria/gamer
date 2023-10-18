e8ae7059d69d304de902daaf07d87c18f71fbd9a
zakariaesprit <zakaria.ayachi@esprit.tn>
2023-10-18 19:15:54 +0100

    .7

--- app/src/main/java/tn/example/gamer/HomeActivity.kt
+++ app/src/main/java/tn/example/gamer/HomeActivity.kt
@@ -1,9 +1,12 @@
 package tn.example.gamer
 
+import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import tn.example.gamer.databinding.HomeActivityBinding
 
 class HomeActivity: AppCompatActivity() {
@@ -11,7 +14,24 @@ class HomeActivity: AppCompatActivity() {
     private lateinit var binding: HomeActivityBinding
 
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = HomeActivityBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val toolbar = findViewById<Toolbar>(R.id.app_bar)
+        setSupportActionBar(toolbar)
+        val navView: BottomNavigationView = binding.bottomNavigation
+        binding.bt1.setOnClickListener{
+            goToFragment(NewsFragment())
+        }
 
+
+        binding.bt2.setOnClickListener{
+            goToFragment(StoreFragment())
+        }
+
+    }
     private  fun goToFragment(fragment: Fragment){
         fragmentManager = supportFragmentManager
         fragmentManager.beginTransaction().replace(R.id.fragmentContainer,fragment).commit()
--- app/src/main/java/tn/example/gamer/StoreFragment.kt
+++ app/src/main/java/tn/example/gamer/StoreFragment.kt
@@ -1,4 +1,17 @@
 package tn.example.gamer
 
-class StoreFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class StoreFragment: Fragment(R.layout.store_fragment)  {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.store_fragment, container, false)
+    }
 }
